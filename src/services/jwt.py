8a0731b3fc98e6561456d47d75cc60b4538a0c56
Daniel Ransom <dantheman0207@users.noreply.github.com>
2025-04-23 22:24:12 +0200

    remove unused files

--- src/services/jwt.py
+++
@@ -1,8 +0,0 @@
-import jwt
-import datetime
-
-def create_jwt(payload: dict, secret: str):
-    # Set the expiration time for the JWT
-    payload["exp"] = datetime.datetime.utcnow() + datetime.timedelta(days=1)  # 1 day validity
-    token = jwt.encode(payload, secret, algorithm="HS256")
-    return token
